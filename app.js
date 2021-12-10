d5b2836520926077cf00f7f549d2f2b651dc082e
Júnior Moreira <junior@adunicamp.org.br>
2021-12-10 17:09:37 -0300

    Primeira aula de Vue3

---
+++ app.js
@@ -0,0 +1,44 @@
+const app = Vue.createApp({
+  //   template: "<h2>Esse é o template</h2>",
+  data() {
+    return {
+      showBooks: true,
+      books: [
+        { title: "Livro 1", author: "Autor 1", isFav: true },
+        { title: "Livro 2", author: "Autor 2", isFav: false },
+        { title: "Livro 3", author: "Autor 3", isFav: true },
+      ],
+      title: "Um título qualquer",
+      author: "Um cara ai, ou uma cara ai",
+      age: "40",
+      x: 0,
+      y: 0,
+    };
+  },
+
+  methods: {
+    changeTitle() {
+      this.title = "Título mudado por método";
+    },
+
+    changeTitleWithParameter(title) {
+      this.title = title;
+    },
+
+    toogleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+
+    handleEvent(event, data) {
+      console.log(event, event.type, data);
+    },
+
+    handleMousemove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+      //   console.log(this.x, this.y);
+    },
+  },
+});
+
+app.mount("#app");
