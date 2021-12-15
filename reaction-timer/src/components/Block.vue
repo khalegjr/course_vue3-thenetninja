048dc47cb1c5f381f9935c21b5707b794c6c4f5d
Júnior Moreira <junior@adunicamp.org.br>
2021-12-15 10:49:54 -0300

    Trabalhando com ciclo de vida do componente

--- reaction-timer/src/components/Block.vue
+++ reaction-timer/src/components/Block.vue
@@ -1,10 +1,35 @@
 <template>
-  <div class="block">click me</div>
+  <div class="block" v-if="showBlock">click me</div>
 </template>
 
 <script>
   export default {
     props: ["delay"],
+
+    data() {
+      return {
+        showBlock: false,
+      };
+    },
+
+    mounted() {
+      // quando o componente é montado no DOM
+      console.log("component mounted");
+      setTimeout(() => {
+        this.showBlock = true;
+        console.log(this.delay);
+      }, this.delay);
+    },
+
+    updated() {
+      // quando o componente é atualizado no DOM
+      console.log("component updated");
+    },
+
+    unmounted() {
+      // quando o componente é desmontado do DOM
+      console.log("component unmounted");
+    },
   };
 </script>
 
