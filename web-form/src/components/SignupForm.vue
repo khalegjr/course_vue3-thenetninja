099ce080b2003bd066e1c2a2d002993707a46eec
Júnior Moreira <junior@adunicamp.org.br>
2021-12-15 14:31:40 -0300

    Remove múltiplos checkboxes que não serão utilizados

--- web-form/src/components/SignupForm.vue
+++ web-form/src/components/SignupForm.vue
@@ -16,28 +16,12 @@
       <input type="checkbox" v-model="terms" required />
       <label>Accept terms and conditions</label>
     </div>
-
-    <div>
-      <input type="checkbox" value="khaled" v-model="names" />
-      <label>Khaled</label>
-    </div>
-
-    <div>
-      <input type="checkbox" value="joão" v-model="names" />
-      <label>João</label>
-    </div>
-
-    <div>
-      <input type="checkbox" value="lilian" v-model="names" />
-      <label>Lilian</label>
-    </div>
   </form>
 
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
   <p>Role: {{ role }}</p>
   <p>Terms accepted: {{ terms }}</p>
-  <p>Names: {{ names }}</p>
 </template>
 
 <script>
@@ -48,7 +32,6 @@
         password: "",
         role: "",
         terms: false,
-        names: [],
       };
     },
   };
