7f20ad7968418764fa2f11dccd77351461bfd56b
Júnior Moreira (Khaled) <khalegjr@gmail.com>
2021-12-17 15:14:38 -0300

    Limpa a estrutura do projeto e usa nome nas chamadas de rotas

--- ninja-jobs/src/views/About.vue
+++ ninja-jobs/src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis enim
+      in eaque aliquam id maiores qui dolorum minus vero recusandae architecto,
+      ipsum porro delectus nobis commodi nemo nihil facilis placeat consectetur
+      eum repellendus libero iste. Cumque dicta fugiat praesentium deleniti
+      similique rem. Obcaecati laudantium consectetur quae iusto quaerat dolor,
+      eos doloribus suscipit eum dolorum eius. Ad iure sunt repellendus? Eaque,
+      sunt iure? Iusto eveniet quisquam asperiores officia ut. Maiores, nobis
+      dolor. Enim quo atque neque in officiis, dolorum magni tempora modi
+      expedita eveniet rem quasi natus placeat, repellendus libero excepturi!
+      Architecto, saepe omnis! Suscipit eveniet facere harum natus, at
+      molestiae.
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis enim
+      in eaque aliquam id maiores qui dolorum minus vero recusandae architecto,
+      ipsum porro delectus nobis commodi nemo nihil facilis placeat consectetur
+      eum repellendus libero iste. Cumque dicta fugiat praesentium deleniti
+      similique rem. Obcaecati laudantium consectetur quae iusto quaerat dolor,
+      eos doloribus suscipit eum dolorum eius. Ad iure sunt repellendus? Eaque,
+      sunt iure? Iusto eveniet quisquam asperiores officia ut. Maiores, nobis
+      dolor. Enim quo atque neque in officiis, dolorum magni tempora modi
+      expedita eveniet rem quasi natus placeat, repellendus libero excepturi!
+      Architecto, saepe omnis! Suscipit eveniet facere harum natus, at
+      molestiae.
+    </p>
   </div>
 </template>
--- ninja-jobs/src/views/Home.vue
+++ ninja-jobs/src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis enim
+      in eaque aliquam id maiores qui dolorum minus vero recusandae architecto,
+      ipsum porro delectus nobis commodi nemo nihil facilis placeat consectetur
+      eum repellendus libero iste. Cumque dicta fugiat praesentium deleniti
+      similique rem. Obcaecati laudantium consectetur quae iusto quaerat dolor,
+      eos doloribus suscipit eum dolorum eius. Ad iure sunt repellendus? Eaque,
+      sunt iure? Iusto eveniet quisquam asperiores officia ut. Maiores, nobis
+      dolor. Enim quo atque neque in officiis, dolorum magni tempora modi
+      expedita eveniet rem quasi natus placeat, repellendus libero excepturi!
+      Architecto, saepe omnis! Suscipit eveniet facere harum natus, at
+      molestiae.
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis enim
+      in eaque aliquam id maiores qui dolorum minus vero recusandae architecto,
+      ipsum porro delectus nobis commodi nemo nihil facilis placeat consectetur
+      eum repellendus libero iste. Cumque dicta fugiat praesentium deleniti
+      similique rem. Obcaecati laudantium consectetur quae iusto quaerat dolor,
+      eos doloribus suscipit eum dolorum eius. Ad iure sunt repellendus? Eaque,
+      sunt iure? Iusto eveniet quisquam asperiores officia ut. Maiores, nobis
+      dolor. Enim quo atque neque in officiis, dolorum magni tempora modi
+      expedita eveniet rem quasi natus placeat, repellendus libero excepturi!
+      Architecto, saepe omnis! Suscipit eveniet facere harum natus, at
+      molestiae.
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: "Home",
+    components: {},
+  };
 </script>
