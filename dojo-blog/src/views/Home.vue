d25f71c4542c2c900d26c1cbbd64b014829712b5
Júnior Moreira (Khaled) <khalegjr@gmail.com>
2021-12-21 20:27:37 -0300

    Como usar refs com composition api

---
+++ dojo-blog/src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    home
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
+  </div>
+</template>
+
+<script>
+  import { ref } from "vue";
+  // @ is an alias to /src
+
+  export default {
+    name: "Home",
+
+    setup() {
+      console.log(this);
+
+      const p = ref(null);
+      console.log(p);
+      console.log(p.value);
+
+      let name = "khaled";
+      let age = 38;
+
+      const handleClick = () => {
+        console.log(p.value);
+        p.value.classList.add("test");
+        p.value.textContent = "hello, ninjas";
+      };
+
+      return { name, age, handleClick, p };
+    },
+  };
+</script>
