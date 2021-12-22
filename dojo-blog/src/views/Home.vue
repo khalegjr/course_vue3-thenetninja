6f7dfce78488f9fcf0205411cc6dc436df530ea1
Júnior Moreira (Khaled) <khalegjr@gmail.com>
2021-12-21 22:21:44 -0300

    Usando computed method com composition api

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -1,35 +1,30 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Refs</h2>
-    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-    <button @click="updateNinjaOne">Update ninja one</button>
-
-    <h2>Reactive</h2>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
-    <button @click="updateNinjaTwo">Update ninja two</button>
+    <input type="text" v-model="search" />
+    <p>seach term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">
+      {{ name }}
+    </div>
   </div>
 </template>
 
 <script>
-  import { ref, reactive } from "vue";
+  import { ref, reactive, computed } from "vue";
   // @ is an alias to /src
 
   export default {
     name: "Home",
 
     setup() {
-      const ninjaOne = ref({ name: "mario", age: 30 });
-      const ninjaTwo = reactive({ name: "luigi", age: 35 });
+      const search = ref("");
+      const names = ref(["mario", "luigi", "toad", "bowser", "koopa", "peach"]);
 
-      const updateNinjaOne = () => {
-        ninjaOne.value.age = 40;
-      };
-      const updateNinjaTwo = () => {
-        ninjaTwo.age = 40;
-      };
+      const matchingNames = computed(() => {
+        return names.value.filter((name) => name.includes(search.value));
+      });
 
-      return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo };
+      return { names, search, matchingNames };
     },
   };
 </script>
