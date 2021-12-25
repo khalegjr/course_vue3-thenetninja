9e0e617288ec0a2ce82600575c5f70c8773db468
Júnior Moreira (Khaled) <khalegjr@gmail.com>
2021-12-25 15:40:44 -0300

    Utilizando props para passar dados entre componentes

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -1,44 +1,30 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <input type="text" v-model="search" />
-    <button @click="handleClick">stop watching</button>
-    <p>seach term - {{ search }}</p>
-    <div v-for="name in matchingNames" :key="name">
-      {{ name }}
-    </div>
+    <post-list :posts="posts" />
   </div>
 </template>
 
 <script>
-  import { ref, computed, watch, watchEffect } from "vue";
+  import { ref } from "vue";
+  import PostList from "../components/PostList.vue";
   // @ is an alias to /src
 
   export default {
+    components: { PostList },
     name: "Home",
 
     setup() {
-      const search = ref("");
-      const names = ref(["mario", "luigi", "toad", "bowser", "koopa", "peach"]);
+      const posts = ref([
+        {
+          title: "welcome to the blog",
+          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio eveniet nostrum aperiam maxime voluptatum quasi, delectus sapiente aut. Fugit maiores ullam, nisi odio blanditiis nam corrupti, assumenda qui neque similique distinctio laudantium, magni sed architecto. Eius vero laborum ad impedit. Officiis enim dolores a dolore, voluptates veniam debitis itaque accusantium. Sapiente vero maxime placeat tenetur qui laborum omnis tempore voluptates nulla rerum nobis quod minus unde, voluptatibus dicta, cumque alias pariatur incidunt esse vitae, nihil ex porro magni. Optio harum vitae, facilis, labore voluptas sequi eligendi temporibus, officia ducimus culpa rem deserunt nobis qui corrupti tempore provident dolorum ullam ipsam.",
+          id: 1,
+        },
+        { title: "top 5 css tips", body: "lorem ipsum", id: 2 },
+      ]);
 
-      const stopWatch = watch(search, () => {
-        console.log("watch function run");
-      });
-
-      const stopEffect = watchEffect(() =>
-        console.log("watchEffect function run", search.value)
-      );
-
-      const matchingNames = computed(() => {
-        return names.value.filter((name) => name.includes(search.value));
-      });
-
-      const handleClick = () => {
-        stopWatch();
-        stopEffect();
-      };
-
-      return { names, search, matchingNames, handleClick };
+      return { posts };
     },
   };
 </script>
